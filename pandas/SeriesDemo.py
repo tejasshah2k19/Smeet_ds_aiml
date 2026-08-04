86ca34fadc941ba185812878ee1c98651b2bd7f3
Tejas Shah <tejasshah2k19@gmail.com>
2026-08-04 19:43:15 +0530

    numyp pandas

---
+++ pandas/SeriesDemo.py
@@ -0,0 +1,34 @@
+import pandas as pd 
+import numpy as np 
+
+s1 = pd.Series([10,20,30,40,50])
+print(s1)
+
+list = [10,"ram",False,25.52]
+s2 = pd.Series(list)
+print(s2)
+
+
+t = ("jack","rock","sakira")
+s3 = pd.Series(t)
+print(s3)
+
+d = { "name":"ram","salary":15000,"city":"Ayodhya"}
+s4 = pd.Series(d)
+print(s4)
+
+
+n1 = np.array([11,22,33,44,55])
+s5 = pd.Series(n1)
+print(s5)
+
+
+print(s1.dtype)
+print(s2.dtype)
+print(s3.dtype)
+print(s4.dtype)
+print(s5.dtype)
+ 
+ 
+s6 = pd.Series([11,22,33],index=[7,8,9])
+print(s6) 
